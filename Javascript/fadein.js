e52300821b1f51e6a17011c77968fba582509fad
heatherosej <64219148+heatherosej@users.noreply.github.com>
2021-03-07 16:55:56 -0800

    indexfade

--- Javascript/fadein.js
+++ Javascript/fadein.js
@@ -1,27 +1,5 @@
-$(function(){  // $(document).ready shorthand
-    $('.ethics').fadeIn('slow');
-  });
-  
-  $(document).ready(function() {
-      
-      /* Every time the window is scrolled ... */
-      $(window).scroll( function(){
-      
-          /* Check the location of each desired element */
-          $('.hideme').each( function(i){
-              
-              var bottom_of_object = $(this).position().top + $(this).outerHeight();
-              var bottom_of_window = $(window).scrollTop() + $(window).height();
-              
-              /* If the object is completely visible in the window, fade it it */
-              if( bottom_of_window > bottom_of_object ){
-                  
-                  $(this).animate({'opacity':'1'},1500);
-                      
-              }
-              
-          }); 
-      
-      });
-      
-  });
+function FadeIn(el2){
+    var element2 = document.getElementById(el2);
+    element2.style.transition = "opacity 0";
+    element2.style.opacity = 1;                
+}
